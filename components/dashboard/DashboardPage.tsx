7c102b918a63ded8b1be086e875d9c20cddec546
Faridul Haque Murshed <faridmurshed9@gmail.com>
2025-12-11 15:51:56 +0600

    icon removed from dashboard data

--- components/dashboard/DashboardPage.tsx
+++ components/dashboard/DashboardPage.tsx
@@ -58,25 +58,7 @@ function DashboardPage() {
       <div className="h-auto lg:h-[calc(100vh-100px)] w-full grid grid-cols-1 lg:grid-cols-2 gap-6 p-4">
         <div className="grid grid-cols-2 lg:grid-cols-2 gap-4 place-items-center bg-base-200 rounded-xl p-4 shadow">
           <div className="flex flex-col items-center justify-center gap-1 p-4 rounded-lg bg-base-100 w-full shadow-sm">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-7 text-primary"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75Z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25Z"
-              />
-            </svg>
+           
             <span className="font-medium text-base-content">Product</span>
             <span className="text-xl font-bold text-primary">
               {dbData?.productsCount}
@@ -84,20 +66,7 @@ function DashboardPage() {
           </div>
 
           <div className="flex flex-col items-center justify-center gap-1 p-4 rounded-lg bg-base-100 w-full shadow-sm">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-7 text-success"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664"
-              />
-            </svg>
+          
             <span className="font-medium text-base-content">Units</span>
             <span className="text-xl font-bold text-success">
               {dbData?.totalUnits}
@@ -105,20 +74,7 @@ function DashboardPage() {
           </div>
 
           <div className="flex flex-col items-center justify-center gap-1 p-4 rounded-lg bg-base-100 w-full shadow-sm">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-7 text-warning"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437"
-              />
-            </svg>
+            
             <span className="font-medium text-base-content">Sold</span>
             <span className="text-xl font-bold text-warning">
               {dbData?.totalSoldUnits}
@@ -126,20 +82,7 @@ function DashboardPage() {
           </div>
 
           <div className="flex flex-col items-center justify-center gap-1 p-4 rounded-lg bg-base-100 w-full shadow-sm">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-7 text-accent"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182"
-              />
-            </svg>
+           
             <span className="font-medium text-base-content">Earnings</span>
             <span className="text-xl font-bold text-accent">
               {dbData?.totalEarning}
